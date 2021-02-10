248d44f754ff2768b5b37f3dee57eee6a50bc684
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-02-10 13:23:04 +0530

    added dog model and template

--- housemanager/models.py
+++ housemanager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+from datetime import datetime
 
 # Create your models here.
+
+
+class Dogs(models.Model):
+    dog_name = models.CharField(max_length=200)
+    dog_birthdate = models.DateTimeField(auto_now=False, auto_now_add=False)
+    dog_weight = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.dog_name}"
--- housemanager/views.py
+++ housemanager/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Dogs
 
 def index(request):
-    return HttpResponse('Hello')
+    dogs = Dogs.objects.all()
+    return render(request, 'housemanager/index.html', {
+        "dogs": dogs
+    })
     
