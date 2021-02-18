db1537fbe1d382180de658f9d7df5e077669078c
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-02-18 14:21:23 +0530

    added register

--- housemanager/urls.py
+++ housemanager/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
     path("dogs", views.dogs, name="dogs"), 
     path("baby", views.baby, name="baby"),
     
