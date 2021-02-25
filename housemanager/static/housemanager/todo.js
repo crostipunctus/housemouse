5b661f9c19ee04f41d82114b79291f5b8de72c6d
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-02-25 08:17:15 +0530

    form added to todolist

--- housemanager/static/housemanager/todo.js
+++ housemanager/static/housemanager/todo.js
@@ -3,8 +3,30 @@ document.addEventListener('DOMContentLoaded', function() {
   let create_todo = document.querySelector("#create-todo")
   
     create_todo.onclick = function () {
-        document.querySelector("#todolist").style.background = "red";
-        console.log("hello")
+        
+      let list = document.querySelector("#ullist");
+      let f = document.createElement("form");
+      f.setAttribute('method',"post");
+      f.setAttribute('action',"submit.php");
+
+      let i = document.createElement("input"); //input element, text
+      i.setAttribute('type',"text");
+      i.setAttribute('name',"username");
+
+      let s = document.createElement("input"); //input element, Submit button
+      s.setAttribute('type',"submit");
+      s.setAttribute('value',"Submit");
+
+      
+      f.classList.add("form-group")
+      console.log(f)
+
+      f.appendChild(i)
+      f.appendChild(s)
+      list.appendChild(f)
+
+
+
     }
 
 
