a55839ff62f4143e2884698bfd6c5944878a275c
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-03-24 12:27:53 +0530

    adding js to baby

--- housemanager/static/housemanager/baby.js
+++ housemanager/static/housemanager/baby.js
@@ -1,2 +1,17 @@
 
 
+document.addEventListener('DOMContentLoaded', function() {
+
+let baby_name = document.querySelector("#baby_name")
+let date = document.querySelector("#due_date")
+
+let add_baby = document.querySelector("#submit_baby")
+
+add_baby.onclick = function () {
+    
+}
+
+
+
+
+})
